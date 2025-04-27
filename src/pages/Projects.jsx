17208766d106ff8f17e85d404f50621133ad5884
Faraandev <fraannoor4@gmail.com>
2025-04-27 14:13:27 +0500

    Portfolio

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+import { CTA } from "../components";
+import { projects } from "../constants";
+import { arrow } from "../assets/icons";
+
+const Projects = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        My{" "}
+        <span className='blue-gradient_text drop-shadow font-semibold'>
+          Projects
+        </span>
+      </h1>
+
+      <p className='text-slate-500 mt-2 leading-relaxed'>
+        I've embarked on none projects yet maybe you can give me one? 😅
+      </p>
+
+      <CTA />
+    </section>
+  );
+};
+
+export default Projects;
